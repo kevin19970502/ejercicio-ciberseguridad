157c9e99c8f4243b9da36229fc7132e05339c980
kevin19970502 <89942531+kevin19970502@users.noreply.github.com>
2024-07-12 08:37:57 -0500

    Update sockett.py

--- sockett.py
+++ sockett.py
@@ -8,15 +8,15 @@ def main():
 	port_max=int(input("Ingresa el puerto mayor: "))
 	print ("-"*20)
 	print("La IP es :"+target)
-	print ("Inicio de escaneos"+str(datetime.now()))#obtenemos la fecha hora convertimos a string para mostralo
+	print ("Inicio de escaneos"+str(datetime.now()))
 	print("-"*20)
 	
 	for port in range (port_min,port_max):
-		s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#SOCKET AF_INET (USAR IPV4), STREAM (usar puerto tcp)
-		socket.setdefaulttimeout(1) # tiempo de espera en un segundo se condiera muerto
+		s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1) 
 		result=s.connect_ex((target,port))
 		if result==0:
-			print("el puerto {} se encuentra abierto".format(port))# formato ingresamos el valor de puerto y lo agregamos dentro de las llaves
+			print("el puerto {} se encuentra abierto".format(port))
 			s.close()
 if __name__== '__main__':
 	try:
